00bcc58e64ae35ef327022b93524b51693726221
Razor101 <aviralsingh99@gmail.com>
2022-06-05 19:44:57 +0530

    upgrade

--- TPO_website/settings.py
+++ TPO_website/settings.py
@@ -82,6 +82,10 @@ DATABASES = {
         'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
+import dj_database_url
+
+db_from_env=dj_database_url.config()
+DATABASES['default'].update(db_from_env)
 
 
 # Password validation
@@ -120,7 +124,22 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
+import django_heroku
+# https://docs.djangoproject.com/en/2.2/howto/static-files/
+
+PROJECT_ROOT = os.path.dirname(os.path.abspath(__file__))
+
+# Static files (CSS, JavaScript, Images)
+# https://docs.djangoproject.com/en/1.9/howto/static-files/
+STATIC_ROOT = os.path.join(PROJECT_ROOT, 'staticfiles')
 STATIC_URL = '/static/'
+
+# Extra places for collectstatic to find static files.
+STATICFILES_DIRS = (
+    os.path.join(PROJECT_ROOT, 'static'),
+)
+
+django_heroku.settings(locals())
 STATICFILES_DIR= os.path.join(BASE_DIR,'static')
 MEDIA_URL='/media/'
 MEDIA_ROOT= os.path.join(BASE_DIR,'media_cdn')
